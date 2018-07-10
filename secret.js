373933b38cdc0fd9506a0ee82dd5e57ad0ed04e8
omarASC5 <ocobas19@avenues.org>
2018-07-10 16:34:57 -0400

    Help me find these secrets!

--- secret.js
+++ secret.js
@@ -1,11 +1,2 @@
 console.log("There are 8 secrets in total.");
 console.log("This doesn't count as one.");
-
-console.log("HIIIIIIIII MY NAME IS OMAR");
-console.log("AHHHHHH");
-
-console.log("*********************************");
-
-console.log("WHAT'S UP DOC???????");
-
-console.log("This is the last one!!");
