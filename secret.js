ef5da3c98824ef451ac502ab55c2889c288e3732
omarASC5 <ocobas19@avenues.org>
2018-07-10 16:19:29 -0400

    new yelling comments

--- secret.js
+++ secret.js
@@ -2,3 +2,6 @@ console.log("There are 8 secrets in total.");
 console.log("This doesn't count as one.");
 
 console.log("HIIIIIIIII MY NAME IS OMAR");
+console.log("AHHHHHH");
+
+console.log("*********************************");
